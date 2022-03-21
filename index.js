72dd0264743007ab8dbce933b8319afea05c5827
Rabi-Mezan <ravimezan@gmail.com>
2022-03-21 09:26:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 5000
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
+
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krqaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+
+        await client.connect()
+        console.log('db connected');
+
+        const database = client.db('skilline');
+        const newsCollection = database.collection('News')
+        const testimonialsCollection = database.collection('Testimonials')
+
+
+        // get api for recommended news
+        app.get('/news', async (req, res) => {
+            const result = await newsCollection.find({}).toArray()
+            res.send(result)
+        })
+        // get api for recommended testimonials
+        app.get('/testimonials', async (req, res) => {
+            const result = await testimonialsCollection.find({}).toArray()
+            res.send(result)
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("hello from server");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
